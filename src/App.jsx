98b7f0c71593008d62d80af5bfc34f2966a03cac
Rahul Jindal <rahuljindal1803@gmail.com>
2023-08-27 17:18:20 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,56 @@
+import Alert from "./Alert";
+import Navebar from "./Navebar";
+import TextForm from "./TextForm";
+import React, { useState } from "react";
+// import About from "./About";
+// import { Routes, Route } from "react-router-dom";
+function App() {
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#294244";
+      showAlert("Dark mode has been enabled", "Success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "Success");
+    }
+  };
+  return (
+    <>
+      <Navebar
+        title="App.io"
+        mode={mode}
+        toggleMode={toggleMode}
+        about="About Us"
+      />
+      <Alert alert={alert} />
+      <div className="container">
+        <TextForm
+          showAlert={showAlert}
+          heading="enter your text here.."
+          mode={mode}
+          my-3
+        />
+        {/* <Routes>
+          <Route exact path="/" element={} />
+          <Route exact path="/about" element={<About />} />
+        </Routes> */}
+        {/* <About /> */}
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/TextForm.jsx
@@ -0,0 +1,82 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const [Text, setText] = useState("");
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
+  const handleClick = () => {
+    const newText = Text.toUpperCase();
+
+    setText(newText);
+    props.showAlert("Converted to Upper Case", "success");
+  };
+  const handleLoClick = () => {
+    const newText = Text.toLowerCase();
+
+    setText(newText);
+    props.showAlert("Converted to Lower Case", "success");
+  };
+  return (
+    <>
+      <div
+        className="container"
+        style={{ color: props.mode === "dark" ? "white" : "#294244" }}
+      >
+        <h1>{props.heading}</h1>
+        <div
+          className="container "
+          style={{
+            backgroundColor: props.mode === "dark" ? "#294244" : "white",
+          }}
+        >
+          <textarea
+            className="form-control"
+            id="myBox"
+            rows="8"
+            value={Text}
+            onChange={handleChange}
+            style={{
+              backgroundColor: props.mode === "dark" ? "#294244" : "white",
+              color: props.mode === "dark" ? "white" : "#294244",
+            }}
+          ></textarea>
+        </div>
+
+        <button
+          type="button"
+          className="btn btn-primary my-2 mx-2"
+          onClick={handleClick}
+        >
+          convert to upperCase{" "}
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary my-2 mx-2"
+          onClick={handleLoClick}
+        >
+          convert to LowerCase{" "}
+        </button>
+        <div
+          className="container "
+          style={{
+            backgroundColor: props.mode === "dark" ? "#294244" : "white",
+          }}
+        >
+          <h2>Your Text Summary</h2>
+          <p>
+            {
+              Text.split(" ").filter(function(num) {
+                return num !== "";
+              }).length
+            }
+            words and {Text.length}characters
+          </p>
+          <p>{0.008 * Text.split(" ").length} Minutes read</p>
+          <h2>Preview</h2>
+          <p>{Text}</p>
+        </div>
+      </div>
+    </>
+  );
+}
